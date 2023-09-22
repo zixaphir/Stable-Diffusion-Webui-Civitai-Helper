b3979774746ac15aa2b5ecc606b2216789c89ba8
zixaphir <zixaphirmoxphar@gmail.com>
2023-09-22 09:07:50 -0700

    v1.7.3

--- scripts/ch_lib/util.py
+++ scripts/ch_lib/util.py
@@ -12,7 +12,7 @@ from packaging.version import parse as parse_version
 SHORT_NAME = "sd_civitai_helper"
 
 # current version of the exension
-VERSION = "1.7.2"
+VERSION = "1.7.3"
 
 # Civitai INFO files below this version will regenerated
 COMPAT_VERSION_CIVITAI = "1.7.2"
