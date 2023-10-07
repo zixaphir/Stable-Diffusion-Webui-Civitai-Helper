e3f2bba9aed368b1892bf182d854439cebe55c9a
zixaphir <zixaphirmoxphar@gmail.com>
2023-10-06 20:15:41 -0700

    v1.7.4

--- scripts/ch_lib/util.py
+++ scripts/ch_lib/util.py
@@ -17,13 +17,13 @@ from packaging.version import parse as parse_version
 SHORT_NAME = "sd_civitai_helper"
 
 # current version of the exension
-VERSION = "1.7.3"
+VERSION = "1.7.4"
 
 # Civitai INFO files below this version will regenerated
 COMPAT_VERSION_CIVITAI = "1.7.2"
 
 # SD webui model info JSON below this version will be regenerated
-COMPAT_VERSION_SDWEBUI = "1.7.2"
+COMPAT_VERSION_SDWEBUI = "1.7.4"
 
 def_headers = {
     "User-Agent": (
