847168566f48318456d36e6320254f39f3b97480
aylz10 <aylz10@126.com>
2023-10-07 15:59:00 +0800

    添加新按钮（Update image generation information(Experimental)）

--- scripts/civitai_helper.py
+++ scripts/civitai_helper.py
@@ -138,11 +138,18 @@ def on_ui_tabs():
                             value=model_types
                         )
 
+
                 # with gr.Row():
                 scan_model_civitai_btn = gr.Button(
                     value="Scan",
                     variant="primary",
                     elem_id="ch_scan_model_civitai_btn"
+                )
+
+                Scan_civitai_info_image_meta_btn = gr.Button(
+                    value="Update image generation information(Experimental)",
+                    variant="primary",
+                    elem_id="ch_Scan_civitai_info_image_meta_btn"
                 )
                 # with gr.Row():
                 scan_model_log_md = gr.Markdown(
@@ -328,6 +335,11 @@ def on_ui_tabs():
                 scan_model_types_drop, max_size_preview_ckb,
                 nsfw_preview_threshold_drop, refetch_old_ckb
             ],
+            outputs=scan_model_log_md
+        )
+
+        Scan_civitai_info_image_meta_btn.click(
+            model.Scan_civitai_info_image_meta,
             outputs=scan_model_log_md
         )
 
