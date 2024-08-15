eff9db0b57cfcc9a5d1fab91981b0d5e7e2889cc
zixaphir <zixaphirmoxphar@gmail.com>
2024-08-15 16:42:31 -0500

    v1.8.11

--- ch_lib/util.py
+++ ch_lib/util.py
@@ -25,7 +25,7 @@ except ModuleNotFoundError:
 SHORT_NAME = "sd_civitai_helper"
 
 # current version of the exension
-VERSION = "1.8.10"
+VERSION = "1.8.11"
 
 # Civitai INFO files below this version will regenerated
 COMPAT_VERSION_CIVITAI = "1.7.2"
