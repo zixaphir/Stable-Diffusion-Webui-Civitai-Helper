f5200ad5187bad2ad2e48e6a0240f256bb8315e4
ComplexRobot <complexrobotx@gmail.com>
2024-10-20 03:02:31 -0700

    Implement special parsing rules.

--- scripts/image_metadata.py
+++ scripts/image_metadata.py
@@ -2,6 +2,7 @@ import json
 import re
 import os
 from pathlib import Path
+from functools import reduce
 
 from ch_lib import util
 from modules import script_callbacks, extra_networks, prompt_parser
@@ -60,12 +61,27 @@ def add_resource_metadata(params):
 
     # Add textual inversion embed metadata
     if len(embed_filepaths) > 0:
-        embed_regex = re.compile(r"(?:^|[\s,])(" + '|'.join(re.escape(embed_name) for embed_name in embed_filepaths.keys()) + r")(?:$|[\s,])", re.IGNORECASE | re.MULTILINE)
-        for prompt in [sd_processing.prompt, sd_processing.negative_prompt]:
-            # strip lora definitions, calculate attention weights
-            for text, weight in prompt_parser.parse_prompt_attention(extra_networks.parse_prompt(prompt)[0]):
-                for match in embed_regex.findall(text):
-                    add_civitai_resource(embed_filepaths[match.lower()], weight, "embed")
+        embed_weights = {}
+        try:
+            embed_regex = re.compile(r"(?:^|[\s,])(" + '|'.join(re.escape(embed_name) for embed_name in embed_filepaths.keys()) + r")(?:$|[\s,])", re.IGNORECASE | re.MULTILINE)
+            for prompt in [sd_processing.prompt, sd_processing.negative_prompt]:
+                # parse all special prompt rules
+                extra_networks_stripped, _ = extra_networks.parse_prompt(prompt)
+                _, cond_rules_applied, _ = prompt_parser.get_multicond_prompt_list([extra_networks_stripped])
+                prompt_edit_schedule = prompt_parser.get_learned_conditioning_prompt_schedules(cond_rules_applied, sd_processing.steps)
+                prompts = [text for step, text in reduce(lambda list1, list2: list1 + list2, prompt_edit_schedule)]
+                for scheduled_prompt in prompts:
+                    # calculate attention weights
+                    for text, weight in prompt_parser.parse_prompt_attention(scheduled_prompt):
+                        for match in embed_regex.findall(text):
+                            # store final weight of embedding in dictionary
+                            embed_weights[match.lower()] = weight
+        except Exception as e:
+            util.printD(f"Error parsing prompt for embeddings: {e}")
+
+        # add final weights for embeddings
+        for embed_name, weight in embed_weights.items():
+            add_civitai_resource(embed_filepaths[embed_name], weight, "embed")
 
     if len(civitai_resource_list) > 0:
         params.pnginfo['parameters'] += f", Civitai resources: {json.dumps(civitai_resource_list, separators=(',', ':'))}"
